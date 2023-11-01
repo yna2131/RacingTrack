86b24b504cdf85e98fbe6b45fcf4215e84c132dc
yna2131 <jn7924@gmail.com>
2023-11-01 15:18:07 -0600

    feat(Vehicle): Set speed to the vehicle

--- AvoidObstacles/Assets/Scripts/PlayerController.cs
+++ AvoidObstacles/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Mover vehiculo hacia adelante
-        transform.Translate(0,0,1);
+        // transform.Translate(0,0,1);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
