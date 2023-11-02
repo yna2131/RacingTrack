1c62e3ec2d1df97fd4ae83546a64977277d08195
yna2131 <jn7924@gmail.com>
2023-11-02 10:23:24 -0600

    feat(scene): Move vehicle with keyboard and obstacles added

--- AvoidObstacles/Assets/Scripts/FollowPlayer.cs
+++ AvoidObstacles/Assets/Scripts/FollowPlayer.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This follow controller class will update the events from the main camera
+/// Standar coding documentarion can be found in
+/// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/documentation-comments
+/// </summary>
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 6, -7);
-    // Start is called before the first frame update
+
+    /// <summary>
+    /// This method is called before the first frame update
+    /// </summary>
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// This method is called once per frame
+    /// </summary>
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
--- AvoidObstacles/Assets/Scripts/PlayerController.cs
+++ AvoidObstacles/Assets/Scripts/PlayerController.cs
@@ -2,21 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This player controller class will update the events from the vehicle player
+/// Standar coding documentarion can be found in
+/// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/documentation-comments
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
     // Velocidad del vehiculo
     public float speed = 5.0f;
-    // Start is called before the first frame update
+    public float turnSpeed = 0.0f;
+    public float horizontalInput;
+    public float forwardInput;
+
+    /// <summary>
+    /// This method is called before the first frame update
+    /// </summary>
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// This method is called once per frame
+    /// </summary>
     void Update()
     {
-        // Mover vehiculo hacia adelante
-        // transform.Translate(0,0,1);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
